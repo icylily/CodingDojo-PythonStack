3013790bbde6f4d267d2a6c526e83ea9332f3ab1
icylily <icylily0116@gmail.com>
2019-02-19 11:16:54 -0800

    add filter on drop down list

--- django/django_orm/book_authors_proj/apps/books_authors_app/models.py
+++ django/django_orm/book_authors_proj/apps/books_authors_app/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=255)
     desc = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Authors(models.Model):
+class Author(models.Model):
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length=255)
     notes = models.TextField(null=True)
-    books = models.ManyToManyField(Books, related_name="authors")
+    books = models.ManyToManyField(Book, related_name="authors")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- django/django_orm/book_authors_proj/apps/books_authors_app/views.py
+++ django/django_orm/book_authors_proj/apps/books_authors_app/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render,HttpResponse,redirect
-from .models import Books,Authors
+from .models import Book,Author
 
 # Create your views here.
 def index(request):
     # return HttpResponse("books_authors_app.index")
     context = {
-        "all_the_books": Books.objects.all()
+        "all_the_books": Book.objects.all()
     }
     return render(request, "books_authors_app/books.html",context)
 def process(request):
     if request.POST['wich_place'] == 'add_book':
         this_title = request.POST['title']
         this_desc = request.POST['desc']
-        Books.objects.create(title=this_title,desc=this_desc)
+        Book.objects.create(title=this_title,desc=this_desc)
         return redirect('/')
     elif request.POST['wich_place'] == 'add_author_to_book':
-        this_book = Books.objects.get(id= int(request.POST['book_id']))
-        this_author = Authors.objects.get(id=int(request.POST['author']))
+        this_book = Book.objects.get(id= int(request.POST['book_id']))
+        this_author = Author.objects.get(id=int(request.POST['author']))
         this_book.authors.add(this_author)
         path = "/books/"+str(request.POST['book_id'])
         return redirect(path)
@@ -24,11 +24,11 @@ def process(request):
         this_first_name = request.POST['first_name']
         this_last_name = request.POST['last_name']
         this_notes = request.POST['notes']
-        Authors.objects.create(first_name=this_first_name,last_name = this_last_name,notes= this_notes)
+        Author.objects.create(first_name=this_first_name,last_name = this_last_name,notes= this_notes)
         return redirect('/authors')
     elif request.POST['wich_place'] == 'add_book_to_author':
-        this_book = Books.objects.get(id=int(request.POST['book']))
-        this_author = Authors.objects.get(id=int(request.POST['author_id']))
+        this_book = Book.objects.get(id=int(request.POST['book']))
+        this_author = Author.objects.get(id=int(request.POST['author_id']))
         this_book.authors.add(this_author)
         path = "/authors/"+str(request.POST['author_id'])
         return redirect(path)
@@ -36,24 +36,26 @@ def process(request):
         return redirect('/')
 
 def show_book(request,number):
-    this_book = Books.objects.get(id=int(number))
+    this_book = Book.objects.get(id=int(number))
     context = {
         "book":this_book,
-        "authors_list":Authors.objects.all(),
+        "authors_list":Author.objects.exclude(books__id = int(number)),
     }
     return render(request,"books_authors_app/book_detail.html",context)
 
 def authors(request):
     context = {
-        "all_the_authors": Authors.objects.all()
+        "all_the_authors": Author.objects.all()
     }
     return render(request, "books_authors_app/authors.html", context)
 
 
 def show_author(request, number):
-    this_author = Authors.objects.get(id=int(number))
+    this_author = Author.objects.get(id=int(number))
+    
     context = {
         "author": this_author,
-        "books_list": Books.objects.all(),
+        # "books_list": Book.objects.all(),
+        "books_list": Book.objects.exclude(authors__id=int(number)),
     }
     return render(request, "books_authors_app/author_detail.html", context)
